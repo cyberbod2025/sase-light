031616c6a72fa30a2df75cce75a92fc5d8241f4e
HUGO SÁNCHEZ RESÉNDIZ <hugo.watson.primero@gmail.com>
2026-07-05 01:29:52 -0600

    fix(sase): replace println toast with real Snackbar

--- composeApp/src/commonMain/kotlin/com/example/util/ToastUtil.kt
+++ composeApp/src/commonMain/kotlin/com/example/util/ToastUtil.kt
@@ -1,14 +1,7 @@
 package com.example.util
 
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.compositionLocalOf
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 typealias ToastFunction = (String) -> Unit
 
-val LocalToast = compositionLocalOf<ToastFunction> { { message -> println("Toast: $message") } }
-
-fun toast(scope: CoroutineScope, message: String) {
-    scope.launch { println("Toast: $message") }
-}
+val LocalToast = compositionLocalOf<ToastFunction> { { } }
